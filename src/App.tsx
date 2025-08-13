288aa6e7863ad3c1a137fc4d20b577e7a3698f1b
fabiansilvera <fabiansilvera@gmail.com>
2025-08-13 16:14:58 -0300

    Capa funcional de la app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react"
+import { useEffect, useMemo, useReducer } from "react"
 import Form from "./components/Form"
 import {activityReducer, initialState } from "./reducers/activityReducer"
 import ActivityList from "./components/ActivityList"
@@ -7,6 +7,12 @@ function App() {
 
   const [state, dispatch] = useReducer(activityReducer, initialState)
 
+  useEffect(() => {
+    localStorage.setItem('activities', JSON.stringify(state.activities))
+  }, [state.activities])
+
+  const canRestartApp = () => useMemo(() => state.activities.length, [state.activities])
+
   return (
     <>
       <header className="bg-lime-600 py-3">
@@ -14,12 +20,19 @@ function App() {
             <h1 className="text-center text-lg font-bold text-white uppercase">
               Contador de Calorias
             </h1>
+
+            <button className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10" disabled={!canRestartApp} onClick={() => dispatch({type: 'restart-app'})}>
+                Reiniciar App
+            </button>
           </div>
       </header>
 
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form dispatch={dispatch} />
+          <Form 
+            dispatch={dispatch}
+            state={state}
+          />
         </div>
       </section>
 
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,14 @@
-import { useState} from "react"
+import { useState, useEffect} from "react"
 import type {ChangeEvent, Dispatch, FormEvent} from "react"
 import { v4 as uuidv4 } from "uuid"
 import type { Activity } from "../types"
 import { categories } from "../data/categories"
-import type { ActivityActions } from "../reducers/activityReducer"
+import type { ActivityActions, ActivityState } from "../reducers/activityReducer"
 
 
 type FormProps = {
-    dispatch : Dispatch<ActivityActions>
+    dispatch : Dispatch<ActivityActions>,
+    state: ActivityState
 }
 
 const initialState : Activity = {
@@ -17,9 +18,16 @@ const initialState : Activity = {
         calories: 0
     }
 
-export default function Form({dispatch} : FormProps) {
+export default function Form({dispatch, state} : FormProps) {
     // Inicializa las activities vacio
     const [activity, setActivity] = useState<Activity>(initialState)
+
+    useEffect(() => {
+        if(state.activeId) {
+            const selectedActivity = state.activities.filter( stateActivity => stateActivity.id === state.activeId)[0]
+            setActivity(selectedActivity)
+        }
+    }, [state.activeId])
     
     const handleChange = (e : ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
         const isNumberField = ['category', 'calories'].includes(e.target.id)
